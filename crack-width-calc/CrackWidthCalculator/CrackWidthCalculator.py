50adca2b8fb043784aa762fe37bedbe50cb737bb
viktar_b <viktar.beliakou@gmail.com>
2021-03-15 15:40:07 +0000

    SectionProperties added

--- crack-width-calc/CrackWidthCalculator/CrackWidthCalculator.py
+++ crack-width-calc/CrackWidthCalculator/CrackWidthCalculator.py
@@ -3,51 +3,182 @@ import numpy as np
 import xlwings as xw
 
 @xw.func
-def get_crack_width(
-    cnom_tension,
-    cnom_compression, 
-    c_dur, 
-    moment 
-):
+def get_crack_width(c_nom_tension, c_nom_compression, c_dur, moment,
+    rf_diameter_first_layer = 0,
+    rf_spacing_first_layer = 0,
+    rf_diameter_second_layer = 0,
+    rf_spacing_second_layer = 0,
+    rf_diameter_third_layer = 0,
+    rf_spacing_third_layer = 0):
+
     """
     To activate a function in the excel file, press "Import Functions" 
     in the xlwings tab 
     """
-    crack_width = 1 
+
+    section_properrties = SectionProperties(c_nom_tension, c_nom_compression, c_dur,
+        rf_diameter_first_layer,
+        rf_spacing_first_layer,
+        rf_diameter_second_layer,
+        rf_spacing_second_layer,
+        rf_diameter_third_layer,
+        rf_spacing_third_layer
+    )
+    
+
+    serviceability_checks = ServiceabilityChecks(section_properrties, moment)
+    crack_width_calc = CrackWidthCalc(serviceability_checks)
+
+    crack_width = crack_width_calc.get_crack_width()
     return crack_width 
 
-class ConcreteSection: 
+
+class CrackWidthCalc: 
+
     """
-    Standard units: 
-        dimensions - mm
-        stresses - N/mm^2 or MPa 
-        time - days 
-  
-    TODO 
-    Standard parameters are assumed to be constant at this stage 
-    Write setters and getters for the fields below to verify dimesions are valid 
-    Calcs are for slab only, refactor to include beams 
+    Preparation - bottom-up calcualtions for the crack width 
+
+    w_k = s_rmax*(e_sm - e_cm)
+
+    s_rmax for long term loading
+        if spacing > 5 (c + diam/2)
+            s_rmax = 1.3*(h-x)      comm: x - d_c cocnrete depth in compression 
+        else 
+            s_rmax = k_3*c + k_1*k_2*k_4*diam/rho_eff   comm: 
+                                                            c - cover to bar controlling crack for crackwidth calculations 
+                                                            rho_eff = A_s/A_c,eff
+
+    (e_sm - e_cm) for long term loading  
+
+        max(0,6*(sigma_s/E_s ), (sigma_s - k_t*f_ct_eff/rho_p_eff*(1 + alpha_e*rho_eff))/E_s)     comm:
+
+                                        k_t = 0.4
+                                        f_ct_eff = 3.2 MPa for f_ck = 35 
+                                        alpha_e = E_s / E_cm = 200,000 / 34.077 for f_ck = 35 
+                                        d - depth to centroid F77 = sum(A_s_layer*d_layer)/A_s_total 
+                                        
+                                        To be determined: 
+                                            rho_eff = A_s/A_ceff
+                                                A_ceff = h_ceff*width 
+                                                    h_ceff = max( min(
+                                                                        (h' - d_c)/3,  
+                                                                        2.5*(h-d), 
+                                                                        h/2)
+                                                                        ), 
+                                                                bar_group)  =   c_dur + d_shear_bar + d_first_layer + 
+                                                                                dist_between_layers (if second is present) +  
+                                                                                d_second_layer 
+                                                                                + aggregate_size + 5 TODO??
+                                            d_c_lt - F192 = 
+                                            d_c_st - F175
+                                            I_cracked 
+                                        
+                                        
     """
-    _aggregate_size = 20 
-    _width = 1000
 
-
-
-    def __init__(self, depth):
-        """
-        depth - thickness of the section 
-        width - to be taken as 1000 mm if slab is 
-
-        """
-        self.depth = depth 
+    def __init__(self, section_properrties):
+        self.section_properties = section_properrties
         
+    def get_crack_width(self, c_nom_tension, c_nom_compression, c_dur, moment):
+        self.c_nom_tension = c_nom_tension
+        self.c_nom_compression = c_nom_compression 
+        self.c_dur = c_dur
+        self.moment = moment
 
-class CrackWidth: 
-    pass 
+        s_rmax = self.get_s_rmax()
+        e_sm = self.get_e_sm()
+        e_cm = self.e_cm()
+
+        crack_width = s_rmax*(e_sm - e_cm)
+
+        return crack_width
+
+    def get_s_rmax():
+        pass
+
+    def get_e_sm():
+        pass
+
+    def get_e_cm():
+        pass
+
+class ServiceabilityChecks: 
+
+    def __init__(self, section_properties):
+        self.sections_properties = section_properties
+        self.longterm_sigma_s = self.get_longterm_sigma_s()
+
+    def get_longterm_sigma_s():
+        """                       
+        For pure bending:
+            sigma_s = M_sls_permanent*10^6*(d - d_c_lt)/I_cracked + M_sls_permanent*10^6*(d - d_c_st)/I_cracked
+        
+        Permanent moment is not considered at this stage         
+        """
+
+
+        return 
 
 
 class SectionProperties: 
-    pass
+    
+    #PROJECT SPECIFIC SECTION PROPERTIES (to be moved to init)
+    depth = 750 
+    width = 1000 
+    f_yd = 500
+    f_ck = 35
+    spacing_between_rf = 25
+
+    def __init__(self, c_nom_tension, c_nom_compression, c_dur,
+                        rf_diameter_first_layer, rf_spacing_first_layer,
+                        rf_diameter_second_layer, rf_spacing_second_layer,
+                        rf_diameter_third_layer, rf_spacing_third_layer):
+        
+        self.c_nom_tension = c_nom_tension
+        self.c_nom_compression = c_nom_compression
+        self.c_dur = c_dur
+
+        self.rf_diameter_first_layer = rf_diameter_first_layer
+        self.rf_spacing_first_layer = rf_spacing_first_layer
+        self.rf_diameter_second_layer = rf_diameter_second_layer
+        self.rf_spacing_second_layer = rf_spacing_second_layer
+        self.rf_diameter_third_layer = rf_diameter_third_layer
+        self.rf_spacing_third_layer = rf_spacing_third_layer
+        
+        self._area_1 = self.get_single_layer_steel_area(self.rf_diameter_first_layer, self.rf_spacing_first_layer)
+        self._area_2 = self.get_single_layer_steel_area(self.rf_diameter_second_layer, self.rf_spacing_second_layer) 
+        self._area_3 = self.get_single_layer_steel_area(self.rf_diameter_third_layer, self.rf_spacing_third_layer)
+        
+        self.steel_area_tension = self.get_steel_area_tension()
+        self.depth_to_centroid = self.get_depth_to_centroid()
+
+        self.steel_properties = SteelProperties(self.f_yd)
+        self.concrete_properties = ConcreteProperties(self.f_ck)
+
+        self.depth_to_neutral_axis = self.depth / 2
+        self.second_moment_of_area =  self.width * self.depth**3 / 12 
+
+    def get_steel_area_tension(self):
+        return self._area_1 + self._area_2 + self._area_3
+
+    def get_depth_to_centroid(self):
+        d_1 = self.depth - self.c_nom_tension - self.rf_diameter_first_layer/2   
+        d_2 = d_1 - self.rf_diameter_first_layer/2  - self.spacing_between_rf - self.rf_diameter_second_layer/2 
+        d_3 = d_2 - self.rf_diameter_second_layer/2 - self.spacing_between_rf - self.rf_diameter_third_layer/2
+
+        return (d_1*self._area_1 + \
+                d_2*self._area_2 + \
+                d_3*self._area_3) / self.steel_area_tension
+    
+    def get_single_layer_steel_area(self, diameter, spacing):
+        if diameter != 0:
+            return (np.pi*diameter**2 / 4) * (self.width / spacing)
+        else:
+            return 0
+    
+    def to_string(self):
+        return f"steel_area_tension = {self.steel_area_tension:0.2f}\n" + \
+                f"depth_to_centroid = {self.depth_to_centroid:0.2f}\n"
 
 
 class ConcreteProperties: 
@@ -66,6 +197,7 @@ class ConcreteProperties:
 
     def __init__(self, f_ck):
         self.f_ck = f_ck
+
         self.f_av = self.get_f_av()
         self.f_cm = self.get_f_cm()
         self.f_ctm = self.get_f_ctm()
@@ -109,60 +241,18 @@ class ConcreteProperties:
                 f"f_ctd = {self.f_ctd:0.2f}\n"    
 
     
-
-
-
-    
-
 class SteelProperties: 
-    pass
+    
+    E_s = 200000
+    gamma_s = 1.15 
 
+    def __init__(self, f_yk):
+        self.f_yk = f_yk 
 
+        self.f_yd = f_yk / self.gamma_s 
 
-
-
-
-
-"""
-Preparation - bottom-up calcualtions for the crack width 
-
-w_k = s_rmax*(e_sm - e_cm)
-
-s_rmax for long term loading
-    if spacing > 5 (c + diam/2)
-        s_rmax = 1.3*(h-x)      comm: x - d_c cocnrete depth in compression 
-    else 
-        s_rmax = k_3*c + k_1*k_2*k_4*diam/rho_eff   comm: 
-                                                        c - cover to bar controlling crack for crackwidth calculations 
-                                                        rho_eff = A_s/A_c,eff
-
-(e_sm - e_cm) for long term loading  
-
-    max(0,6*(sigma_s/E_s ), (sigma_s - k_t*f_ct_eff/rho_p_eff*(1 + alpha_e*rho_eff))/E_s)     comm:
-
-                                    k_t = 0.4
-                                    f_ct_eff = 3.2 MPa for f_ck = 35 
-                                    alpha_e = E_s / E_cm = 200,000 / 34.077 for f_ck = 35 
-                                    d - depth to centroid F77 = sum(A_s_layer*d_layer)/A_s_total 
-                                    
-                                    For pure bending:
-                                        sigma_s = M_sls_permanent*10^6*(d - d_c_lt)/I_cracked + M_sls_permanent*10^6*(d - d_c_st)/I_cracked
-                                    
-                                    To be determined: 
-                                        rho_eff = A_s/A_ceff
-                                            A_ceff = h_ceff*width 
-                                                h_ceff = max( min(
-                                                                    (h' - d_c)/3,  
-                                                                    2.5*(h-d), 
-                                                                    h/2)
-                                                                    ), 
-                                                            bar_group)  =   c_dur + d_shear_bar + d_first_layer + 
-                                                                            dist_between_layers (if second is present) +  
-                                                                            d_second_layer 
-                                                                            + aggregate_size + 5 TODO??
-                                        d_c_lt - F192 = 
-                                        d_c_st - F175
-                                        I_cracked 
-                                       
-                                        
-    """
+    def to_string(self):
+        return f"E_s = {self.E_s:0.2f}\n" + \
+                f"gamma_s = {self.gamma_s:0.2f}\n" + \
+                f"f_yk = {self.f_yk:0.2f}\n" + \
+                f"f_yd = {self.f_yd:0.2f}\n"
